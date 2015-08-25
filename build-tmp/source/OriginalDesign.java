f8f1130a5f79af544dd6e6e5eb8152c52eea15e8
sky121 <skylarooz@gmail.com>
2015-08-25 08:05:47 -0700

    made some changes

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -1,14 +1,30 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
 float size = 50;
 float size2 = 10;
 int bg = 150;
 
-void setup()
+public void setup()
 {
 
 	size(1000,800);
 	background(155, 0, 50);
 }
-void draw()
+public void draw()
 {
 
 background(10, 150, 0);
@@ -33,7 +49,7 @@ background(10, 150, 0);
 
 }
 
-void mousePressed(){
+public void mousePressed(){
 		background(10, 0, 150);
 	ellipse(mouseX,mouseY,200,200);
 	ellipse(mouseX-40,mouseY-30,50,50);
@@ -51,3 +67,12 @@ void mousePressed(){
 
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
